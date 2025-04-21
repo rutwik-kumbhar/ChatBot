22117c0db20bb07777664f3c7837fba55ad9dece
Manish Jaiswal <manishjaiswal@monocept.com>
2025-04-21 12:50:16 +0530

    changes done for application properties

--- chatbot-service/chatbot-service/src/main/resources/data.sql
+++ chatbot-service/chatbot-service/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO placeholder (id, name, icon) VALUES
 (1, 'Ask about medical insurance', 'U+1F3A5'),
 (2, 'Ask about HR related queries', 'U+2764 U+FE0F'),
-(3, 'Ask about policy related queries', 'U+1F697'),
+(3, 'Ask about policy related queries', 'U+1F697')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO option (id, name, icon) VALUES
