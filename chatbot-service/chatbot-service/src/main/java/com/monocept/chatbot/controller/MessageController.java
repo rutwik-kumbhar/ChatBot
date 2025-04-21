1a1c7756aa507abdda2ddd4b56ef0d33391c92ad
ShivaniArun <shivaniarun2515@gmail.com>
2025-04-21 19:01:56 +0530

    "Changed Url"

--- chatbot-service/chatbot-service/src/main/java/com/monocept/chatbot/controller/MessageController.java
+++ chatbot-service/chatbot-service/src/main/java/com/monocept/chatbot/controller/MessageController.java
@@ -12,9 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/chatbot")
 public class MessageController {
 
     private final MessageService messageService;
