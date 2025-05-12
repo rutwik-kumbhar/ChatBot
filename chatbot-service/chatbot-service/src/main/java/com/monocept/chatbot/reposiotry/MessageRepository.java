95cb69cdad66f9e22cdbfb7668864d163a5244a3
rutwikkumbhar <rutwikkumbhar@monocept.com>
2025-05-12 10:29:54 +0530

    count api

--- chatbot-service/chatbot-service/src/main/java/com/monocept/chatbot/reposiotry/MessageRepository.java
+++ chatbot-service/chatbot-service/src/main/java/com/monocept/chatbot/reposiotry/MessageRepository.java
@@ -2,6 +2,8 @@ package com.monocept.chatbot.reposiotry;
 
 import com.monocept.chatbot.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,4 +18,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 //    @Query("SELECT new com.monocept.chatbot.model.dto.HistoryDTO(c.msgId, c.msg, c.messageTo, c.dateTime, c.replyId, c.type, c.mediaUrl, c.activity) FROM History c WHERE c.dateTime >= :dateTime AND c.email = :email order by dateTime DESC" )
 //    Page<HistoryDTO> findMessagesFromLast90DayswrtEmail(@Param("dateTime") LocalDateTime dateTime, @Param("email") String email, Pageable pageable );
 
+
+    @Query(value = "SELECT COUNT(*) FROM message WHERE message_to = 'BOT' AND status = 'DELIVERED' AND user_id = :userId", nativeQuery = true)
+    long countDeliveredBotMessagesByAgentId(String userId);
 }
---
+++ chatbot-service/chatbot-service/src/main/java/com/monocept/chatbot/utils/AppConstant.java
@@ -0,0 +1,7 @@
+package com.monocept.chatbot.utils;
+
+public class AppConstant {
+
+    public  static  final  String SUCCESS = "success";
+    public static  final String FAILURE = "failure";
+}
